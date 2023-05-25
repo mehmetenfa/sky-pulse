e61eb640cdb34141ab99cc46cda5be76cfb9f3fb
qxan <mehmet.enfa@gmail.com>
2023-05-25 16:40:29 +0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="min-w-screen h-[4rem] flex items-center justify-center">
+      <h3 className="text-HFtextColor font-semibold text-lg">Copyright © SkyPulse all rights reserved. Powered by Mehmet Enfa.</h3>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
